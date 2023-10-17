acd83f33a7436bd66ba0e846770d92f848942672
dangduoc97 <dangvanduoc1997@gmail.com>
2023-10-18 00:43:56 +0700

    move proto outside

--- dist/accountParser/accountParser.js
+++ dist/accountParser/accountParser.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPublicKey = exports.createAny = exports.accountParser = void 0;
-var core_proto_ts_1 = require("@injectivelabs/core-proto-ts");
+var cjs_1 = require("../core-proto-ts/cjs");
 var accountParser = function (ethAccount) {
-    var account = core_proto_ts_1.InjectiveTypesV1Beta1Account.EthAccount.decode(ethAccount.value);
+    var account = cjs_1.InjectiveTypesV1Beta1Account.EthAccount.decode(ethAccount.value);
     var baseAccount = account.baseAccount;
     var pubKey = baseAccount.pubKey;
     return {
@@ -20,7 +20,7 @@ var accountParser = function (ethAccount) {
 };
 exports.accountParser = accountParser;
 var createAny = function (value, type) {
-    var message = core_proto_ts_1.GoogleProtobufAny.Any.create();
+    var message = cjs_1.GoogleProtobufAny.Any.create();
     message.typeUrl = type;
     message.value = value;
     return message;
@@ -35,18 +35,19 @@ var getPublicKey = function (_a) {
     var path;
     var baseProto;
     if (chainId.startsWith('injective')) {
-        proto = core_proto_ts_1.CosmosCryptoSecp256k1Keys.PubKey.create();
-        baseProto = core_proto_ts_1.CosmosCryptoSecp256k1Keys.PubKey;
-        path = '/injective.crypto.v1beta1.ethsecp256k1.PubKey';
+        proto = cjs_1.CosmosCryptoSecp256k1Keys.PubKey.create();
+        baseProto = cjs_1.CosmosCryptoSecp256k1Keys.PubKey;
+        // path = '/injective.crypto.v1beta1.ethsecp256k1.PubKey';
+        path = '/cosmos.crypto.secp256k1.PubKey';
     }
     else if (chainId.startsWith('evmos')) {
-        proto = core_proto_ts_1.CosmosCryptoSecp256k1Keys.PubKey.create();
-        baseProto = core_proto_ts_1.CosmosCryptoSecp256k1Keys.PubKey;
+        proto = cjs_1.CosmosCryptoSecp256k1Keys.PubKey.create();
+        baseProto = cjs_1.CosmosCryptoSecp256k1Keys.PubKey;
         path = '/ethermint.crypto.v1.ethsecp256k1.PubKey';
     }
     else {
-        proto = core_proto_ts_1.CosmosCryptoSecp256k1Keys.PubKey.create();
-        baseProto = core_proto_ts_1.CosmosCryptoSecp256k1Keys.PubKey;
+        proto = cjs_1.CosmosCryptoSecp256k1Keys.PubKey.create();
+        baseProto = cjs_1.CosmosCryptoSecp256k1Keys.PubKey;
         path = '/cosmos.crypto.secp256k1.PubKey';
     }
     proto.key = Buffer.from(key, 'base64');
